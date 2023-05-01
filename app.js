f0514956105b5dd7ccaba35573b4671013ead3f4
kalen510 <kalen.wiley@gmail.com>
2023-05-01 14:38:57 -0400

    further validate user input fixes #3

--- app.js
+++ app.js
@@ -57,9 +57,23 @@ const promptUser = () => {
             }
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enteer some more information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some imformation about yourself:',
+            // object below is being destructured to see how the user answered the previous prompt
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
