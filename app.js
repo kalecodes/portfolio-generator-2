09eb601ea62b32839bad124f52a6e968c9a98136
kalen510 <kalen.wiley@gmail.com>
2023-05-01 13:59:09 -0400

    first inquirer prompt

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 // value change in the block is reflected throughout the function, i.e., outside the block.
 // let is block-scoped, so changing its value in a block does not change its value outside the block _if_ the variable is not redeclared in the block
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// ----------------------------------------------------------------------
+// const profileDataArgs = process.argv.slice(2);
 
 // const printProfileData = profileDataArr => {
 //     // This...
@@ -23,14 +25,30 @@ const profileDataArgs = process.argv.slice(2);
 // const github = profileDataArgs[1];
 
 // same as above, using assignment destructuring
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
+// -----------------------------------------------------------------------
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+
+// const pageHTML = generatePage(name, github);
 
 // The first argument is the name of the file that's being created. 
 // The next argument is the data that will write onto the file, in this case the HTML template literal. 
 // The last parameter is a callback function that will be used for error handling and success messages
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    // create an exception and stop the execution of the code
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     // create an exception and stop the execution of the code
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
